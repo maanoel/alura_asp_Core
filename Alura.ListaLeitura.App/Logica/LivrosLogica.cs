81be2155b6f653ad788e20765199156404843a85
Manoel Vitor Brito Moura <manoel.vitor@bh01.local>
2020-06-29 00:22:50 -0300

    reflection para roteamento padrão asp.net core

---
+++ Alura.ListaLeitura.App/Logica/LivrosLogica.cs
@@ -0,0 +1,62 @@
+﻿using Alura.ListaLeitura.App.HTML;
+using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App.Logica
+{
+    public class LivrosController
+    {
+        private static string CarregaLista(IEnumerable<Livro> livros)
+        {
+            var conteudoArquivo = HtmlUtils.CarregaArquivoHTML("lista");
+            foreach (var livro in livros)
+            {
+                conteudoArquivo = conteudoArquivo
+                    .Replace("#NOVO-ITEM#", $"<li>{livro.Titulo} - {livro.Autor}</li>#NOVO-ITEM#");
+            }
+            return conteudoArquivo.Replace("#NOVO-ITEM#", "");
+        }
+
+        public static Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var html = CarregaLista(_repo.ParaLer.Livros);
+            return context.Response.WriteAsync(html);
+        }
+
+        public static Task LivrosLendo(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var html = CarregaLista(_repo.Lendo.Livros);
+            return context.Response.WriteAsync(html);
+        }
+
+        public static Task LivrosLidos(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var html = CarregaLista(_repo.Lidos.Livros);
+            return context.Response.WriteAsync(html);
+        }
+
+        public string Detalhes(int id)
+        {
+            var repo = new LivroRepositorioCSV();
+            var livro = repo.Todos.First(l => l.Id == id);
+            return livro.Detalhes();
+        }
+
+        public string Teste()
+        {
+            return "nova funcionalidade implementada";
+        }
+
+    }
+}
